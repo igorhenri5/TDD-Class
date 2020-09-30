b3fe6de86594f538363c88bdde03c89eb356102b
Igor Henrique <igorhenri5@hotmail.com>
2020-09-30 16:27:49 -0300

    Step 4

--- Dollar.java
+++ Dollar.java
@@ -1,9 +1,9 @@
 class Dollar extends Money{
-   Dollar(int amount) {
-      this.amount= amount;
-   }
-   Money times(int multiplier)  {
-      return new Dollar(amount * multiplier);
-   }
-
+  Dollar(int amount, String currency)  {
+    super(amount, currency);
+  }
+  
+  Money times(int multiplier)  {
+    return Money.dollar(amount * multiplier);
+  }
 }
--- Money.java
+++ Money.java
@@ -1,18 +1,29 @@
 abstract class Money  {
-   protected int amount;
+  protected int amount;
+  protected String currency;
    
-   static Money dollar(int amount)  {
-      return new Dollar(amount);
-   }
+  static Money dollar(int amount)  {
+    return new Dollar(amount, "USD");
+  }
    
-   static Money franc(int amount) {
-      return new Franc(amount);
-    }
-	
-   abstract Money times(int multiplier);
+  static Money franc(int amount) {
+    return new Franc(amount, "CHF");
+  }
 
-   public boolean equals(Object object) {
-      Money money = (Money) object;
-      return amount == money.amount && getClass().equals(money.getClass());
-   }   
+  Money(int amount, String currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+	
+  abstract Money times(int multiplier);
+
+  public boolean equals(Object object) {
+    Money money = (Money) object;
+    return amount == money.amount && getClass().equals(money.getClass());
+  }   
+
+  String currency() {
+    return currency;
+  }
+
 }
